16d268946a58834d4ab17216ca897d7b94c967b9
taeyun <lwe12f675@gmail.com>
2024-05-23 18:56:13 +0900

    useEffect 라이프 사이클

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -2,10 +2,20 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export default function Detail() {
+  console.log("----------------");
+  console.log("Detail 컴포넌트 실행");
   const params = useParams();
   console.log("params:", params);
   useEffect(() => {
     console.log("Detail 컴포넌트 마운트 이후 실행");
+    return () => {
+      console.log("Detail 클린업함수 실행");
+    };
   }, []);
-  return <div>Detail</div>;
+  return (
+    <div>
+      {console.log("return Detail 실행")}
+      Detail
+    </div>
+  );
 }
